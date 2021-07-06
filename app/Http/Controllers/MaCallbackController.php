9e39a1aeb84d13bc105ff4dc358636e98dd2fe36
Aung Thura <aungthurawin@mzonlinemarket.com>
2021-07-06 13:26:11 +0630

    update Ma Callback Controller

--- app/Http/Controllers/MaCallbackController.php
+++ app/Http/Controllers/MaCallbackController.php
@@ -21,6 +21,7 @@ class MaCallbackController extends Controller
 
     public function callback(Request $request) {
     	$callback = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        \Log::info($callback);
         
 		$this->reqBody = $callback;
 		$data = $request->all();
